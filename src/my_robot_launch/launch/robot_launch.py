9979b863ab30ce67620b53ac264df653d3e523c1
namayri-gif <namayri@etgah.com>
2026-06-05 18:59:39 +0400

    Update robot_launch.py

--- src/my_robot_launch/launch/robot_launch.py
+++ src/my_robot_launch/launch/robot_launch.py
@@ -6,50 +6,10 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     # Get package directories
     my_package_dir = get_package_share_directory('my_robot_launch')
-    rviz_config_file = os.path.join(my_package_dir, 'rviz', 'main_view.rviz')
+    rviz_config_file = os.path.join(my_package_dir, 'rviz', 'robot_launch.rviz')
   
     # List of nodes to launch
     nodes = [
-        # Your robot's motor controller
-        Node(
-            package='my_robot_driver',
-            executable='motor_controller',
-            name='motor_controller',
-            output='screen',
-            parameters=[
-                {'max_speed': 1.0},
-                {'max_acceleration': 0.5}
-            ]
-        ),
-  
-        # Your robot's sensor reader
-        Node(
-            package='my_robot_driver',
-            executable='sensor_reader',
-            name='sensor_reader',
-            output='screen'
-        ),
-  
-        # Main robot controller
-        Node(
-            package='my_robot_control',
-            executable='main_controller',
-            name='main_controller',
-            output='screen',
-            remappings=[
-                ('cmd_vel', '/robot/cmd_vel')
-            ]
-        ),
-  
-        # TF2 broadcaster from external package
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_tf',
-            arguments=['0', '0', '0.1', '0', '0', '0', 'base_link', 'laser_link'],
-            output='screen'
-        ),
-  
         # RViz visualization
         Node(
             package='rviz2',
